d57225830e43d636ce07ab57f6200e13e0b71ba9
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2023-01-11 13:47:37 +0100

    jv

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+
+
+const Client = new Discord.Client(
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+)
+
+//login discord bot
+Client.login('MTA2MjcwMjUxMTg1NDcxOTAwNg.Gz5i-G.BUYx5SpjeyiFN_ig2DjclRn6-JybyTQc49BAzE')
